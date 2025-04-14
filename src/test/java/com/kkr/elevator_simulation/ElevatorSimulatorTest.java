6008e08a1a63020c2436243df230ab163ebc4ccb
vvaidya1 <vaibhav.vaidya11@gmail.com>
2025-04-14 22:41:37 +0100

    File renamed

--- src/test/java/com/kkr/elevator_simulation/ElevatorSimulatorTest.java
+++ src/test/java/com/kkr/elevator_simulation/ElevatorSimulatorTest.java
@@ -14,7 +14,7 @@ import java.util.Random;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ElevatorSimulationTest {
+public class ElevatorSimulatorTest {
 
     @Test
     public void testSingleRequestGetsPickedAndDropped() throws IOException {
